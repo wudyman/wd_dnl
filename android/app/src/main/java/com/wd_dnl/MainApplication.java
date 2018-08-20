9ba0438cee6f2ec56b319338807f1869e00cb458
dongxw <wudy.dong@163.com>
2018-08-20 17:49:05 +0800

    update

--- android/app/src/main/java/com/wd_dnl/MainApplication.java
+++ android/app/src/main/java/com/wd_dnl/MainApplication.java
@@ -9,6 +9,10 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.wd_dnl.webview.WebViewReactPackage;
+import com.theweflex.react.WeChatPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +29,12 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new SplashScreenReactPackage()
+          new SplashScreenReactPackage(),
+          new CustomToastPackage(),
+          new WebViewReactPackage(),
+          new WeChatPackage(),
+          new RNDeviceInfo(),
+          new VectorIconsPackage()
       );
     }
 
